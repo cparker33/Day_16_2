34d75ab1f949f4f00fd83a8398dfd4acfc15156e
Caleb <caleb@theparker.me>
2017-03-26 18:39:07 -0700

    Added Filter

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -8,6 +8,7 @@ export default React.createClass({
 
     return {
       displaytype: 'all',
+      displaylist: [],
       tasklist: [],
       taskCnt: 0,
       activeCnt: 0,
@@ -16,6 +17,7 @@ export default React.createClass({
     }
 
   },
+
 
   handleCheck: function (c) {
 
@@ -41,18 +43,44 @@ export default React.createClass({
       }
 
       newArr.push(task)
+
     })
 
     this.setState({
 
       tasklist: newArr,
-      activeCnt: newAct
+      activeCnt: newAct, 
+      totalCnt: newArr.length
+
 
     })    
 
   },
 
+
+  clearComplete: function () {
+
+    var newArr = this.state.tasklist.filter((task) => {
+
+      return task.complete === false
+
+    })
+
+    this.setState({
+
+      tasklist: newArr,
+      displaylist: newArr,
+      totalCnt: newArr.length,
+      activeCnt: newArr.length
+
+    })     
+      
+  },
+
+
   handleRemove: function (c) {
+
+    var actC = 0
 
     var newArr = this.state.tasklist.filter((task) => {
 
@@ -61,18 +89,32 @@ export default React.createClass({
         return true
 
       } 
-  
+        
         return false
+
+    })
+
+    newArr.forEach((task) => {
+
+      if (task.complete === false) {
+
+        actC += 1
+
+      }
 
     })
 
     this.setState({
 
-      tasklist: newArr
+      tasklist: newArr,
+      displaylist: newArr,
+      totalCnt: newArr.length,
+      activeCnt: actC
 
     })    
 
   },
+
 
   addTask: function (c) {
 
@@ -88,15 +130,105 @@ export default React.createClass({
     this.setState({
 
       tasklist: [...this.state.tasklist, newTask],
+      displaylist: [...this.state.displaylist, newTask],
       taskCnt: this.state.taskCnt + 1,
-      activeCnt: this.state.activeCnt + 1
+      activeCnt: this.state.activeCnt + 1,
+      totalCnt: this.state.totalCnt + 1,
 
     })
     
+  },
+
+
+  showAll: function () {
+
+    var newArr = []
+    var actC = 0
+
+    this.state.tasklist.forEach((task) => {
+
+      if (task.complete === false) {
+
+        actC += 1
+
+      }
+
+      newArr.push(task)
+
+    })
+
+    this.setState({
+
+      displaytype: 'all',
+      displaylist: newArr,
+      totalCnt: newArr.length,
+      activeCnt: actC
+
+    }) 
 
   },
 
 
+  showComp: function () {
+
+    var newArr = []
+    var actC = 0
+    
+    this.state.tasklist.forEach((task) => {
+
+      if (task.complete === true) {
+
+        newArr.push(task)
+
+      }
+
+      if (task.complete === false) {
+
+        actC += 1
+
+      }
+
+    })
+
+    this.setState({
+
+      displaytype: 'com',
+      displaylist: newArr,
+      totalCnt: newArr.length,
+      activeCnt: actC
+
+    })  
+
+  },
+
+
+  showAct: function () {
+
+    var newArr = []
+    var actC = 0
+
+    this.state.tasklist.forEach((task) => {
+
+      if (task.complete === false) {
+
+        newArr.push(task)
+          
+        actC += 1
+
+      }
+
+    })
+
+    this.setState({
+
+      displaytype: 'act',
+      displaylist: newArr,
+      totalCnt: newArr.length,
+      activeCnt: actC
+
+    })  
+
+  },
 
   render: function() {
 
@@ -106,56 +238,62 @@ export default React.createClass({
         id="cResults">
 
         {
-          
-          this.state.tasklist.map((task, index) =>  {
 
-            return <Task 
-                    key={index} 
-                    task={task} 
-                    checkClick={this.handleCheck} 
-                    removeClick={this.handleRemove}
-                   />
+        this.state.displaylist.map((task, index) =>  {
+
+          return <Task 
+            key={index} 
+            task={task} 
+            checkClick={this.handleCheck} 
+            removeClick={this.handleRemove}/>
               
-            })
+          })
+
         }
         
-
-
-
-<div 
-  id={this.state.activeCnt > 0 ? 'cFooter' : 'cFooterHidden'}
->
+        <div 
+          id={this.state.tasklist.length > 0 ? 'cFooter' : 'cFooterHidden'}>
   
-  <div 
-    id="cTaskCount">{this.state.activeCnt} {this.state.activeCnt === 1 ? 'task' : 'tasks'} pending</div>
+          <div 
+            id="cTaskCount">
+            {this.state.activeCnt} {this.state.activeCnt === 1 ? 'task' : 'tasks'} pending
+          </div>
 
-    <div 
-      id="cFiltBtnCont"
-    >
+          <div 
+            id="cFiltBtnCont">
   
-      <div>All</div>
-      <div>Active</div>
-      <div>Complete</div>
-    </div>
+          <div 
+            className={this.state.displaytype === 'all' ? 'cSelected' : 'cNotSelected'}
+            onClick={this.showAll}>
+            All
+          </div>
 
-    <div 
-      id="cClear"
-    >Clear Completed
-    </div>
+          <div 
+            className={this.state.displaytype === 'act' ? 'cSelected' : 'cNotSelected'}
+            onClick={this.showAct}>
+            Active
+          </div>
 
-        
+          <div 
+            className={this.state.displaytype === 'com' ? 'cSelected' : 'cNotSelected'}
+            onClick={this.showComp}>
+            Complete
+          </div>
 
-  </div>
+        </div>
 
-
-
-
+        <div 
+          id="cClear"
+          onClick={this.clearComplete}>
+          Clear Completed
+        </div>
 
       </div>
+
+    </div>
 
     )
 
   }
-
 
 })
